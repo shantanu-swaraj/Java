78ed78e5b56a77ba29fbcc04db802ebf4da678ca
Shantanu Swaraj <95582008+shantanu-swaraj@users.noreply.github.com>
2024-11-13 15:29:43 +0530

    Add files via upload

---
+++ countdigits.java
@@ -0,0 +1,12 @@
+public class countdigits {
+    public static void main(String[] args){
+       /* int num = 123456;
+        int count = String.valueOf(num).length();
+        System.out.println("Number of digits in " + num + " is: " + count);
+        */
+
+        String str = "shantanu";
+        int count = str.length();
+        System.out.println("Number of characters in " + str + " is: " + count);
+    }
+}
